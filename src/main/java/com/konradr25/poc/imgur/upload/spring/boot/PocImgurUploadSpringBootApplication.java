e57b235b580f10ee767626ef9080edb7ca3657ca
Konrad Ryczek <konrad.ryczek@sollers.eu>
2017-01-29 04:06:33 +0100

    Repo init

---
+++ src/main/java/com/konradr25/poc/imgur/upload/spring/boot/PocImgurUploadSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.konradr25.poc.imgur.upload.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocImgurUploadSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocImgurUploadSpringBootApplication.class, args);
+	}
+}
---
+++ src/test/java/com/konradr25/poc/imgur/upload/spring/boot/PocImgurUploadSpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.konradr25.poc.imgur.upload.spring.boot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PocImgurUploadSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
